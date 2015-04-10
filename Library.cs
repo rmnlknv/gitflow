b5dd58ecd4407e65feb2b5e0d11cfafb5e498135
rmnlknv <snovix@gmail.com>
2015-04-10 09:36:26 +0300

    Version 0.1

---
+++ Library.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    class Library
+    {
+        static void Find()
+        {
+            Console.WriteLine("FOUND!!!");
+        }
+
+	static void Delete()
+	{
+	    Console.WriteLine("DELETED!");
+	}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yo, yo, yo!!!
+            Console.WriteLine("Hello from truepaca!")
+            Console.WriteLine(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
